1ddc618ca0b7c1be998253ddd869b3f6ff9c1aa6
Taro　Matsuo <taromatsuo0422@yahoo.co.jp>
2022-03-27 18:00:48 +0900

    third-commit

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -12,4 +12,22 @@ class TodoController extends Controller
         $items = Todo::all();
         return view('index', ['items' => $items]);
     }
+    public function create(Request $request)
+    {
+        $this->validate($request, Todo::$rules);
+        $form = $request->all();
+        Todo::create($form);
+        return redirect('/');
+    }
+    public function update(Request $request)
+    {
+        $this->validate($request, Todo::$rules);
+        $form = $request->all();
+        dd($form);
+        unset($form['_token']);
+        return response()->json([
+        'form' => $form
+    ]);
+    }
+
 }
--- database/migrations/2022_03_24_145516_create_todos_table.php
+++ database/migrations/2022_03_24_145516_create_todos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class TodoTable extends Migration
+class CreateTodosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class TodoTable extends Migration
      */
     public function up()
     {
-        Schema::create('todo', function (Blueprint $table) {
+        Schema::create('todos', function (Blueprint $table) {
             $table->id('id',20);
             $table->string('content',191);
             $table->timestamp('created_at')->useCurrent()->nullable();
@@ -28,6 +28,6 @@ class TodoTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('todo');
+        Schema::dropIfExists('todos');
     }
 }
